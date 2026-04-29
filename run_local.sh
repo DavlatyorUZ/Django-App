c2cd2764490980e7c7c69c9b1f964461e063a3fc
Davlatyor <davlatyor.oke@gmail.com>
2026-04-29 07:49:10 +0500

    Faylni o'chirish

--- run_local.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
-python manage.py runserver 127.0.0.1:8000
